5386d0b50f59dead98c59c33f212247c2df4c854
Julius Palalay <julius.palalay@usjr.edu.ph>
2023-02-09 21:59:46 +0800

    HomePage Design

--- lib/pages/Homepage.dart
+++ lib/pages/Homepage.dart
@@ -12,7 +12,7 @@ class HomePage extends StatelessWidget{
           height: 500,
           padding: EdgeInsets.only(top: 15),
           decoration: BoxDecoration(
-            color: Colors.white,
+            color:Color.fromARGB(255, 212, 212, 212),
             borderRadius:BorderRadius.only(
               topLeft: Radius.circular(35),
               topRight: Radius.circular(35),
@@ -32,9 +32,32 @@ class HomePage extends StatelessWidget{
                   margin: EdgeInsets.only(left:5),
                   height: 50,
                   width: 300,
-                  child: TextFormField(),
-                )
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Search Here...",
+                    ),
+                  ),
+                ),
+                Spacer(),
+                Icon(Icons.camera_alt,
+                size:20,
+                color: Color(0xFF4C53A5),
+                ),
               ],),
+            ),
+            Container(
+              alignment: Alignment.centerLeft ,
+              margin: EdgeInsets.symmetric(
+                vertical: 20,
+                horizontal: 10,
+              ),
+              child: Text(
+                "Categories",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold,
+                color: Color(0xFF4C53A5),
+                ),
+              ),
             ),
           ],
           ),
